4ed4787f42369b957cf03d41b35883432262417d
Owais <mohammadowaisraza12@gmail.com>
2026-02-20 12:15:37 +0530

    Updated HTML Code

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/products')
+def products():
+    return render_template('product-list.html')
 
 if __name__ =='__main__':
    app.run(debug=True)
